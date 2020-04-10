ded2e5f680fb9efd8bf23daf8111e13badc35439
michaelwozniak <mj.wozniak9@student.uw.edu.pl>
2020-04-10 17:51:55 +0200

    Rename files

--- project_scrapy/justjoinit_scraper/justjoinit_scraper/items.py
+++ project_scrapy/justjoinit_scraper/justjoinit_scraper/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class PracujplScraperItem(scrapy.Item):
+class JustjoinitScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
